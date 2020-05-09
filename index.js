847965fe2c2ae75c231a3d159ed3f57789cb1789
ThisIsItz <itziarurbieta94@gmail.com>
2020-05-09 18:33:49 +0100

    Added color to pieces

--- index.js
+++ index.js
@@ -38,38 +38,38 @@ function createPiece(type){
         ]
     } else if (type === 'O'){
         return [
-            [1, 1],
-            [1, 1],
+            [2, 2],
+            [2, 2],
         ]
     } else if (type === 'L'){
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1],
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
         ]
     } else if (type === 'J'){
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0]
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0]
         ]
     } else if (type === 'I'){
         return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0]
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0]
         ]
     } else if (type === 'S'){
         return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0]
         ] 
     } else if (type === 'Z'){
         return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0]
         ]
     }
@@ -87,7 +87,7 @@ function drawMatrix(matrix, offset){
     matrix.forEach((row, y) => {
         row.forEach((value,x) => {
             if (value !== 0){
-                context.fillStyle = 'red';
+                context.fillStyle = color[value];
                 context.fillRect(x + offset.x,
                                  y + offset.y,
                                     1, 1)
@@ -184,6 +184,17 @@ function update(time = 0){
     draw()
     requestAnimationFrame(update)
 }
+
+const color = [
+    null,
+    'red',
+    'blue',
+    'violet',
+    'green',
+    'purple',
+    'orange',
+    'pink'
+]
 
 const arena = createMatrix(12, 20)
 
