d4e83a387ed55402ba2d2b5f0781d8bdd36e849b
ThisIsItz <itziarurbieta94@gmail.com>
2020-05-09 15:28:47 +0100

    Collide detected

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const matrix = [
     [0, 1, 0],
 ]
 
+function collide(arena, player){
+    const [m, o] = [player.matrix, player.pos]
+    for (let y = 0; y < m.length; y++){
+        for (let x = 0; x < m[y].length; x++){
+            if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0){
+                return true
+            } 
+        }
+    }
+    return false
+}
+
 function createMatrix(w,h){
     const matrix = []
     while (h--){
@@ -20,6 +32,8 @@ function createMatrix(w,h){
 function draw(){
     context.fillStyle = '#000'
     context.fillRect(0,0, canvas.width, canvas.height)
+
+    drawMatrix(arena, {x: 0, y: 0})
     drawMatrix(player.matrix, player.pos)
 }
 
@@ -36,9 +50,31 @@ function drawMatrix(matrix, offset){
     })
 }
 
+function merge(arena, player){
+    player.matrix.forEach((row, y) => {
+        row.forEach((value,x) => {
+            if(value != 0){
+                arena[y + player.pos.y][x + player.pos.x] = value
+            }
+        })
+    })
+}
+
 function playerDrop(){
     player.pos.y++
+    if (collide(arena, player)){
+        player.pos.y--;
+        merge(arena,player);
+        player.pos.y = 0
+    }
     dropCounter = 0 
+}
+
+function playerMove(dir){
+    player.pos.x += dir
+    if (collide(arena,player)){
+        player.pos.x -= dir
+    }
 }
 
 let dropCounter = 0
@@ -59,7 +95,6 @@ function update(time = 0){
 }
 
 const arena = createMatrix(12, 20)
-console.log(arena), console.table(arena)
 
 const player = {
     pos: {x : 5, y: 5},
@@ -68,9 +103,9 @@ const player = {
 
 document.addEventListener('keydown', event => {
     if(event.keyCode === 37){
-        player.pos.x--
+        playerMove(-1)
     }else if (event.keyCode === 39){
-        player.pos.x++
+        playerMove(1)
     }else if (event.keyCode === 40){
         playerDrop()
     }
