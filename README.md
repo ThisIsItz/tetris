e5c2b76dd5ef8aa12d25030a3f17f1e0ab750446
ThisIsItz <itziarurbieta94@gmail.com>
2020-06-16 11:10:38 +0100

    Added Readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+Tetris game made from scratch using Vanilla JavaScript.
+
+![Tetris image](./img/tetris.png)
--- index.js
+++ index.js
@@ -96,6 +96,7 @@ function createPiece(type){
 function draw(){
     context.fillStyle = '#000'
     context.fillRect(0,0, canvas.width, canvas.height)
+    
 
     drawMatrix(arena, {x: 0, y: 0})
     drawMatrix(player.matrix, player.pos)
